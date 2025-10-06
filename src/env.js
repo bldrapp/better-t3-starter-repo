c00d3f6648f0d916802ec6d637c02acaff650e8b
Mahmoud Nefzi <65515933+Mahmoud-nfz@users.noreply.github.com>
2025-10-06 15:14:12 +0100

    chore: add clerk envs (#7)
    * feat: add clerk auth to project
    
    * protected page and middleware restriction
    
    * protected procedure
    
    * lint
    
    * chore: add clerk envs
    
    * lint

--- src/env.js
+++ src/env.js
@@ -11,6 +11,7 @@ export const env = createEnv({
 		NODE_ENV: z
 			.enum(["development", "test", "production"])
 			.default("development"),
+		CLERK_SECRET_KEY: z.string(),
 	},
 
 	/**
@@ -19,6 +20,7 @@ export const env = createEnv({
 	 * `NEXT_PUBLIC_`.
 	 */
 	client: {
+		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
 		NEXT_PUBLIC_POSTHOG_KEY: z.string().min(1),
 		NEXT_PUBLIC_POSTHOG_HOST: z.string().url(),
 	},
@@ -30,6 +32,8 @@ export const env = createEnv({
 	runtimeEnv: {
 		DATABASE_URL: process.env.DATABASE_URL,
 		NODE_ENV: process.env.NODE_ENV,
+		CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
 		NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
 		NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
 	},
